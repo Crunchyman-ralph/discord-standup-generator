96c1d4c1168cf7659a0a572893a57a1c884158b6
Crunchyman-ralph <ralphkbiz@gmail.com>
2022-10-25 12:32:20 +0200

    fix preview

--- components/Standup/StandupPreview.tsx
+++ components/Standup/StandupPreview.tsx
@@ -1,14 +1,15 @@
 import { Card, Textarea } from "react-daisyui";
+import ReactTextareaAutosize from "react-textarea-autosize";
 import { useGetStandup } from "../../hooks/UseGetStandup";
 
 export const StandupPreview = () => {
   const getStandup = useGetStandup();
 
   return (
-    <Card className="grow">
+    <Card className="grow sticky top-0 bottom-0">
       <Card.Body>
         <div className="flex w-full h-full component-preview p-4 items-center gap-2 font-sans">
-          <Textarea className="w-full" value={getStandup()} readOnly />
+          <ReactTextareaAutosize value={getStandup()} readOnly />
         </div>
       </Card.Body>
     </Card>
